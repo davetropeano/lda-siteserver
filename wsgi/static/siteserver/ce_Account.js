a9df431cafa02e2dc68d2fe545fed5b753b218a1
frankbu <frankb@ca.ibm.com>
2014-04-08 18:07:44 -0400

    use standard LDP containers

--- wsgi/static/siteserver/ce_Account.js
+++ wsgi/static/siteserver/ce_Account.js
@@ -1,7 +1,6 @@
 XSD = 'http://www.w3.org/2001/XMLSchema#'
 RDFS = 'http://www.w3.org/2000/01/rdf-schema#'
 RDF = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
-BP = 'http://open-services.net/ns/basicProfile#'
 DC = 'http://purl.org/dc/terms/'
 CE = 'http://ibm.com/ce/ns#'
 VCARD = 'http://www.w3.org/2006/vcard/ns#'
