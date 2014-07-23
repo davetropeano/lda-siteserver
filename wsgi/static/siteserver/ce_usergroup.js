1b230cacf4b5190368a5622fb97e903e22419810
Mark Archer <marcher@uci.edu>
2014-07-23 15:27:25 -0700

    single usergroup page working
    There was a conflict between what was the top level html and what was a
    template for usergroups since ce_usergroups.html used usergroup.html as
    a template but then usergroup.html was supposed to work as the top level
    html when a usergroup was navigated to directly (e.g. /ac/1.2)

--- wsgi/static/siteserver/ce_usergroup.js
+++ wsgi/static/siteserver/ce_usergroup.js
@@ -1,27 +1,4 @@
-window.siteserver = window.siteserver || {};
 
-window.siteserver.UserGroupViewModel = function() {
-	
-    var self = this;
-	self.jso = {};
-	self.permissions = [];
-	ko.track(self); 
-	
-	self.init = function(jso){
-		self.jso =jso;
-		var perms = [];
-		for(var i in jso.ac_may){
-		    var perm = new siteserver.UserGroupPermissionViewModel();
-		    perm.init(jso.ac_may[i]);
-		    perms.push(perm);
-		}
-		self.permissions = perms;		
-	};	
-	
-	self.showView = function(model) {
-        self.init(model);        
-        return true;
-    }
-
-	return self;
-}
+var model = window.siteserver.UserGroupViewModel();
+model.init(APPLICATION_ENVIRON.initial_simple_jso);
+ko.applyBindings(model);
---
+++ wsgi/static/siteserver/templates/ce_usergroup.js
@@ -0,0 +1,22 @@
+window.siteserver = window.siteserver || {};
+
+window.siteserver.UserGroupViewModel = function() {
+	
+    var self = this;
+	self.jso = {};
+	self.permissions = [];
+	ko.track(self); 
+	
+	self.init = function(jso){
+		self.jso =jso;
+		var perms = [];
+		for(var i in jso.ac_may){
+		    var perm = new siteserver.UserGroupPermissionViewModel();
+		    perm.init(jso.ac_may[i]);
+		    perms.push(perm);
+		}
+		self.permissions = perms;		
+	};
+
+	return self;
+}
