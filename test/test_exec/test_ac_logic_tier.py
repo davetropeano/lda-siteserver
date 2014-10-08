31b7aad77cc0a78706438ff137b5563ad3537236
Mark Archer <marcher@uci.edu>
2014-10-08 11:20:20 -0700

    tests for ac logic tier

--- test/test_exec/test_ac_logic_tier.py
+++ test/test_exec/test_ac_logic_tier.py
@@ -8,27 +8,27 @@ sys.path.append('../../src')
 sys.path.append('../../ test')
 
 import test_utils
-
 import requests, json, jwt
 from rdf_json import URI, BNode, RDF_JSON_Encoder, RDF_JSON_Document, rdf_json_decoder
 from base_constants import RDF, DC, AC, AC_ALL, ADMIN_USER, CE, VCARD, FOAF, ANY_USER, AC_T, AC_R, AC_C, AC_D, AC_W, AC_X
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-HS_HOSTNAME = 'localhost:3005'
-
+TEST_USER = 'http://ibm.com/ce/user/test'
+HS_HOSTNAME = 'localhost:3001'
 ac_container_url = 'http://%s/ac' % HS_HOSTNAME
 account_container_url = 'http://%s/account' % HS_HOSTNAME
 
-def make_headers(type = 'GET', user = None,password = None):
+def make_headers(type='GET', user=None, password=None,modificationCount=None):
     if type not in ('GET', 'POST', 'PATCH', 'DELETE'):
         raise Exception('invalid header type')        
     
     header = {}    
     
     if user == ADMIN_USER: 
-        password = 'our little secret'    
+        pass
     if user is not None:
+        password = 'our little secret'
         encoded_signature = jwt.encode({'user': user}, password, 'HS256')
         header.update({'Cookie': 'SSSESSIONID=%s' % encoded_signature})
     
@@ -39,18 +39,65 @@ def make_headers(type = 'GET', user = None,password = None):
         #this is something about POST Action
         header.update({'ce-post-reason': 'ce-create'})
     elif type == 'PATCH':
-        header.update({'Content-type': 'application/json'})
+        header.update({'Content-type': 'application/rdf+json+ce'})
+        header.update({'CE-ModificationCount': modificationCount})
     
     return header
+
+def test_basic_rest():
+    container_basic_rest_test(ac_container_url)
+
+def container_basic_rest_test(container_url):
+    # test post
+    body = {
+        '' : {
+            RDF+'type': URI(RDF+'test'),
+            DC+'title': 'test post'
+            }
+        }
+    headers = make_headers('POST', ADMIN_USER)
+    r = requests.post(ac_container_url, headers=headers, data=json.dumps(body, cls=RDF_JSON_Encoder), verify=False)
+    assert r.status_code == 201
+    r_doc = RDF_JSON_Document(r)
+    assert r_doc[r_doc.default_subject()][DC+'title'] == 'test post'
+
+    # patch
+    body = {
+        r_doc.default_subject() : {
+            DC+'title': 'test patch'
+            }
+        }
+    headers = make_headers('PATCH', ADMIN_USER,modificationCount=0)
+    r = requests.patch(r_doc.default_subject(), headers=headers, data=json.dumps(body, cls=RDF_JSON_Encoder), verify=False)
+    print r.text
+    assert r.status_code == 200
+
+    # test get
+    body = {}
+    headers = make_headers('GET', ADMIN_USER)
+    r = requests.get(r_doc.default_subject(), headers=headers, data=json.dumps(body, cls=RDF_JSON_Encoder), verify=False)
+    assert r.status_code == 200
+    r_doc = RDF_JSON_Document(r)
+    assert r_doc[r_doc.default_subject()][DC+'title'] == 'test patch'
+
+    # delete
+    body = {}
+    headers = make_headers('DELETE', ADMIN_USER)
+    r = requests.delete(r_doc.default_subject(), headers=headers, data=json.dumps(body, cls=RDF_JSON_Encoder), verify=False)
+    assert r.status_code == 200
+
+    body = {}
+    headers = make_headers('GET', ADMIN_USER)
+    r = requests.get(r_doc.default_subject(), headers=headers, data=json.dumps(body, cls=RDF_JSON_Encoder), verify=False)
+    assert r.status_code == 404
+
 
 def test_get_as_anon():
     headers = make_headers('GET')
     body = {}
     r = requests.get(ac_container_url, headers=headers, data=json.dumps(body, cls=RDF_JSON_Encoder), verify=False)
     print r.text
-    if r.status_code != 200:
-        return False
-    return True
+    assert r.status_code == 200
 
 def test_post_as_anon():    
     headers = make_headers('POST')
@@ -58,57 +105,31 @@ def test_post_as_anon():
     r = requests.post(ac_container_url, headers=headers, data=json.dumps(body, cls=RDF_JSON_Encoder), verify=False)
     print "status code: %s" % r.status_code
     print r.text
-    if r.status_code != 401:        
-        return False
-    return True    
+    assert r.status_code == 401
 
-def test_post_as_admin():
-    # give everyone access to create new accounts    
+def admin_create_account_usergroup():
+    # give everyone access to create new accounts
     body = {
         '' : {
             RDF+'type': URI(AC+'UserGroup'),
             AC+'who' : [
                 URI(ANY_USER)
                 ],
-            AC+'may' : [ URI('#permission_1') ],
+            AC+'may' : [ URI('#permission_1')],
             },
         '#permission_1' : {
             AC+'do' : AC_C,
-            AC+'to' : [URI(account_container_url) ]
+            AC+'to' : [URI(account_container_url)]
             }
         }
     body = {}
     headers = make_headers('POST',ADMIN_USER)
     r = requests.post(ac_container_url, headers=headers, data=json.dumps(body, cls=RDF_JSON_Encoder), verify=False)
-    print "status code: %s" % r.status_code
-    print r.text
-    if r.status_code != 201:        
-        return False
-    return True
+    return r
 
 def test_post_as_user():
-    # give everyone access to create new accounts    
-    body = {
-        '' : {
-            RDF+'type': URI(AC+'UserGroup'),
-            AC+'who' : [
-                URI(ANY_USER)
-                ],
-            AC+'may' : [ URI('#permission_1') ],
-            },
-        '#permission_1' : {
-            AC+'do' : AC_C,
-            AC+'to' : [URI(account_container_url) ]
-            }
-        }
-    body = {}
-    headers = make_headers('POST',ADMIN_USER)
-    r = requests.post(ac_container_url, headers=headers, data=json.dumps(body, cls=RDF_JSON_Encoder), verify=False)
-    print "status code: %s" % r.status_code
-    print r.text
-    if r.status_code != 201:        
-        return False
-    return True
+    pass
+    # TODO: post to ac with TEST_USER
 
 # this is for working with tests while building them
 if __name__ == "__main__":    
--- test/test_exec/test_basic.py
+++ test/test_exec/test_basic.py
@@ -1,50 +1,23 @@
 import requests, json, jwt, pytest
 #from rdf_json import RDF_JSON_Encoder, RDF_JSON_Document, rdf_json_decoder
-account_container_url = 'http://%s/account' % 'localhost'
 
 def test_server_running():
-    test_result = do_get_test("http://localhost:3005/")
-    assert test_result == True
-
+    assert do_get_test("http://localhost:3001/")
 
 def test_account_get():
-	assert do_get_test("http://localhost:3005/account") == True
+    assert do_get_test("http://localhost:3001/account")
 
 def test_ac_get():
-	assert do_get_test("http://localhost:3005/ac") == True
+    assert do_get_test("http://localhost:3001/ac")
 
 def test_mt_get():
-	assert do_get_test("http://localhost:3005/mt") == True
-	
-##account post and delete
-
-def test_account_post():
-	assert do_post_test(account_container_url)==True
-	
-def test_account_delete():
-	assert do_delete_test()==True
-	
-def do_post_test(url):
-	r= requests.post(url, headers={}, verify= False)
-	print "stats code: %s" % r.status_code
-	print r.text
-	if r.status_code != 201:
-		return false
-	return True
+    assert do_get_test("http://localhost:3005/mt")
 
 def do_get_test(url):
-	r = requests.get(url, headers={}, verify=False)
-	print "status code: %s" % r.status_code
-	print r.text
-	if r.status_code != 200:		
-		return False
-	return True
+    r = requests.get(url, headers={}, verify=False)
+    print "status code: %s" % r.status_code
+    print r.text
+    if r.status_code != 200:
+        return False
+    return True
 
-def do_delete_test(url):
-	#Delete something that just got posted
-	requests.delete(url, headers={})
-
-# this is for working with tests while building them 
-if __name__ == "__main__":	
-	#test_server_running()
-	pass
