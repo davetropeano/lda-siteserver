062fec030adb3a2f9b217b6f703b9e23cd31a14c
Mark Archer <marcher@uci.edu>
2014-11-21 11:10:27 -0800

    changed hosting site in tests

--- test/test_exec/test_basic.py
+++ test/test_exec/test_basic.py
@@ -2,16 +2,16 @@ import requests, json, jwt, pytest
 #from rdf_json import RDF_JSON_Encoder, RDF_JSON_Document, rdf_json_decoder
 
 def test_server_running():
-    assert do_get_test("http://localhost:3001/")
+    assert do_get_test("http://hostingsite.localhost:3001/")
 
 def test_account_get():
-    assert do_get_test("http://localhost:3001/account")
+    assert do_get_test("http://hostingsite.localhost:3001/account")
 
 def test_ac_get():
-    assert do_get_test("http://localhost:3001/ac")
+    assert do_get_test("http://hostingsite.localhost:3001/ac")
 
 def test_mt_get():
-    assert do_get_test("http://localhost:3001/mt")
+    assert do_get_test("http://hostingsite.localhost:3001/mt")
 
 def do_get_test(url):
     r = requests.get(url, headers={}, verify=False)
