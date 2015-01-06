2a8c877ba05b8332bc96f18752141ea1665041b5
Mark Archer <marcher@uci.edu>
2015-01-06 08:10:02 -0800

    refactored a bit to make the goal of account tests clearer

--- test/test_exec/test_account_logic_tier.py
+++ test/test_exec/test_account_logic_tier.py
@@ -21,51 +21,65 @@ HS_HOSTNAME = 'hostingsite.localhost:3001'
 ac_container_url = 'http://%s/ac' % HS_HOSTNAME
 account_container_url = 'http://%s/account' % HS_HOSTNAME
 
-TEST1_USER = '%s/test1#owner' % account_container_url
+USER1_URL = '%s/test1#owner' % account_container_url
+USER2_URL = '%s/test2#owner' % account_container_url
 
-def test_basic_crud(account_test1):
+
+def test_basic_crud(account_user1):
     """
-    :param account_test1: this is automatically passed in by py.test.  It is the result of creating test1 account.
+    :param account_user1: this is automatically passed in by py.test.  It is the result of creating test1 account.
     """
 
     # CRUD test on account has to be done a little different since we switch security contexts after create
-    resource_url = account_test1.default_subject()
+    resource_url = account_user1.default_subject()
 
-    # this test both read and update
-    test_helper.update(resource_url, patch_prop=DC+'title', patch_value='my title', username=TEST1_USER)
+    # this tests both read and update
+    test_helper.update(resource_url, patch_prop=DC+'title', patch_value='my title', username=USER1_URL)
 
     # should we be able to delete an account?
 
 
-def test_anon_access(account_test1):
+def test_anon_access_to_test1_account(account_user1):
     """
     :param account_test1: RDF_Document passed in via py.test
     verify that anonymous users can't access test1 account
     """
-    resource_url = account_test1.default_subject()
+    resource_url = account_user1.default_subject()
     test_helper.resource_access_test(
         resource_url, username=None, patch_prop=DC+'title', patch_value='anon update',
         assert_code_read=401, assert_code_update=401, assert_code_delete=401)
 
 
-def test_admin_access(account_test1):
+def test_admin_access_to_test1_account(account_user1):
     """
-    :param account_test1: RDF_Document passed in via py.test
+    :param account_user1: RDF_Document passed in via py.test
     verify that admin user can't access test1 account
     """
-    resource_url = account_test1.default_subject()
+    resource_url = account_user1.default_subject()
     test_helper.resource_access_test(
         resource_url, username=ADMIN_USER, patch_prop=DC+'title', patch_value='admin update',
         assert_code_read=401, assert_code_update=401, assert_code_delete=401)
 
 
-def test_user2_access(account_test1, account_test2):
-    test1_url = account_test1.default_subject()
-    test2_url = account_test2.default_subject()
+def test_user2_access_to_test1_account(account_user1, account_user2):
+    """
+    :param account_user1: RDF_Document passed in via py.test
+    :param account_user2: RDF_Document passed in via py.test
+    verify that one user can't access another users account
+    """
+    resource_url = account_user1.default_subject()
+    test_helper.resource_access_test(
+        resource_url, username=USER2_URL, patch_prop=DC+'title', patch_value='user2 update',
+        assert_code_read=401, assert_code_update=401, assert_code_delete=401)
+
+    resource_url = account_user2.default_subject()
+    test_helper.resource_access_test(
+        resource_url, username=USER1_URL, patch_prop=DC+'title', patch_value='user1 update',
+        assert_code_read=401, assert_code_update=401, assert_code_delete=401)
 
 
 @pytest.fixture(scope="session")
-def account_test1():
+def account_user1():
     body = {
         '': {
             RDF+'type': URI(CE+'Account'),
@@ -92,12 +106,11 @@ def account_test1():
             VCARD+'country-name': 'Spain'
         }
     }
-    test1 = test_helper.create(container_url=account_container_url, post_body=body)
-
-    return test1
+    user1_r_doc = test_helper.create(container_url=account_container_url, post_body=body)
+    return user1_r_doc
 
 @pytest.fixture(scope="session")
-def account_test2():
+def account_user2():
     body = {
         '': {
             RDF+'type': URI(CE+'Account'),
@@ -124,15 +137,14 @@ def account_test2():
             VCARD+'country-name': 'Spain'
         }
     }
-    test2 = test_helper.create(container_url=account_container_url, post_body=body)
-
-    return test2
+    user2_r_doc = test_helper.create(container_url=account_container_url, post_body=body)
+    return user2_r_doc
 
 # this is for working with tests while building them
 if __name__ == "__main__":
     #r_doc = test_helper.read('http://hostingsite.localhost:3001/account/test1', username='test1')
 
-    r_doc = account_test1()
-    #test_basic_crud(r_doc)
-    test_anon_access(r_doc)
+    user1_r_doc = account_user1()
+    user2_r_doc = account_user2()
+    test_anon_access_to_test1_account(user1_r_doc)
     pass
